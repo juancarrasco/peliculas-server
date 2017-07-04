c614672b6bd993e485de2d1855000585804ca456
juancarrasco <juanandrescarrasco91@gmail.com>
2017-07-04 12:09:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# peliculas-server
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/PeliculasController.js
@@ -0,0 +1,11 @@
+/**
+ * PeliculasController
+ *
+ * @description :: Server-side logic for managing peliculas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
